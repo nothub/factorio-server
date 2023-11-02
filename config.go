d23e2f194069f5aa08e52272c66df9c69d5de151
Florian Hübner <code@hub.lol>
2023-11-02 23:28:55 +0100

    flags

--- config.go
+++ config.go
@@ -3,9 +3,15 @@ package main
 import "flag"
 
 var serverDir string
+var factorioUser string
+var factorioToken string
 
 func init() {
 	serverDirP := flag.String("server-dir", "server", "Server base dir and process pwd")
+	factorioUserP := flag.String("factorio-user", "", "factorio.com username")
+	factorioTokenP := flag.String("factorio-token", "", "factorio.com token")
 	flag.Parse()
 	serverDir = *serverDirP
+	factorioUser = *factorioUserP
+	factorioToken = *factorioTokenP
 }
