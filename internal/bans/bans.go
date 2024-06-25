0803a1c4e5338e30d9548ea402be3914508b61ad
Florian Hübner <fhuebner@posteo.de>
2024-06-25 18:29:55 +0200

    ban sources

--- internal/bans/bans.go
+++ internal/bans/bans.go
@@ -2,6 +2,6 @@ package bans
 
 // server-banlist.json
 
-// TODO: sync bans from:
-//     https://getcomfy.eu/api/v1/ban/get/all
-//     https://m45sci.xyz:8443/server-banlist.json
+// TODO: fetch bans from:
+//     https://getcomfy.eu/api/v1/bans  '.[][].username'
+//     https://m45sci.xyz:8443/server-banlist.json  '.[].username'
