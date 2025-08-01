20061293f8e5eafa757f737c983978898cfd585d
Florian Hübner <fhuebner@posteo.de>
2025-08-01 13:38:32 +0200

    use goccy, add webhook vars, read multiple files

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.5
 
 require (
 	github.com/adrg/xdg v0.5.3
+	github.com/goccy/go-yaml v1.18.0
 	github.com/nothub/semver v0.11.1
 	github.com/ulikunitz/xz v0.5.12
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -9,37 +8,37 @@ import (
 	"slices"
 	"strings"
 
-	"gopkg.in/yaml.v3"
+	"github.com/goccy/go-yaml"
 )
 
-var FilePath = "config.yaml"
 var Loaded Configuration
 
 type Configuration struct {
-	WorkDir       string   `yaml:"workdir"`
 	FactorioUser  string   `yaml:"factorio-user"`
 	FactorioToken string   `yaml:"factorio-token"`
+	WebhookChat   string   `yaml:"webhook-chat"`
+	WebhookStatus string   `yaml:"webhook-status"`
 	Mods          []string `yaml:"mods"`
 }
 
 func Load() {
 
-	SetDefaults()
-	ReadFile(FilePath)
-	ReadFlags()
+	ReadFile("/etc/factorio-server/config.yaml")
+	ReadFile(filepath.Join(os.Getenv("HOME"), ".config/factorio-server/config.yaml"))
+	ReadFile(filepath.Join(os.Getenv("PWD"), "server/config.yaml"))
+
 	Clean()
 	Validate()
 
-}
-
-func SetDefaults() {
-	Loaded.WorkDir = "server"
 }
 
 func ReadFile(filePath string) {
 
 	data, err := os.ReadFile(filePath)
 	if err != nil {
+		if os.IsNotExist(err) {
+			return
+		}
 		log.Printf("failed reading config file %s: %v", filePath, err)
 		return
 	}
@@ -47,7 +46,7 @@ func ReadFile(filePath string) {
 	var cfg Configuration
 	err = yaml.Unmarshal(data, &cfg)
 	if err != nil {
-		log.Printf("failed parsing config file %s: %v", filePath, err)
+		log.Fatalf("failed parsing config file %s: %v\n", filePath, err)
 		return
 	}
 
@@ -59,49 +58,21 @@ func ReadFile(filePath string) {
 		Loaded.FactorioToken = cfg.FactorioToken
 	}
 
+	if len(cfg.WebhookChat) > 0 {
+		Loaded.WebhookChat = cfg.WebhookChat
+	}
+
+	if len(cfg.WebhookStatus) > 0 {
+		Loaded.WebhookStatus = cfg.WebhookStatus
+	}
+
 	for _, mod := range cfg.Mods {
 		Loaded.Mods = append(Loaded.Mods, mod)
-	}
-
-	if len(cfg.WorkDir) > 0 {
-		Loaded.WorkDir = cfg.WorkDir
-	}
-}
-
-func ReadFlags() {
-
-	workDirP := flag.String("workdir", "", "server base dir and process pwd")
-	factorioUserP := flag.String("factorio-user", "", "factorio.com username")
-	factorioTokenP := flag.String("factorio-token", "", "factorio.com token")
-	modsP := flag.String("mods", "", "game mods")
-
-	flag.Parse()
-
-	if len(*workDirP) > 0 {
-		Loaded.WorkDir = *workDirP
-	}
-
-	if len(*factorioUserP) > 0 {
-		Loaded.FactorioUser = *factorioUserP
-	}
-
-	if len(*factorioTokenP) > 0 {
-		Loaded.FactorioToken = *factorioTokenP
-	}
-
-	if len(*modsP) > 0 {
-		for _, mod := range strings.Split(*modsP, ",") {
-			if len(mod) > 0 {
-				Loaded.Mods = append(Loaded.Mods, mod)
-			}
-		}
 	}
 
 }
 
 func Clean() {
-
-	Loaded.WorkDir = strings.TrimSpace(Loaded.WorkDir)
 
 	Loaded.FactorioUser = strings.TrimSpace(Loaded.FactorioUser)
 
@@ -116,11 +87,6 @@ func Clean() {
 }
 
 func Validate() {
-
-	_, err := filepath.Abs(Loaded.WorkDir)
-	if err != nil {
-		log.Fatalln(fmt.Sprintf("config workdir invalid: %v", err))
-	}
 
 	if len(Loaded.FactorioUser) < 1 {
 		log.Fatalln(fmt.Sprintf("config missing factorio.com username"))
